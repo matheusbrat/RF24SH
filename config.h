c45a90b429002865818de40bfc484d1a31a0808e
Matheus (X-warrior) Bratfisch <matheusbrat@gmail.com>
2013-02-09 16:00:38 -0200

    HEX, DEC, BIN, OCT defined if not Arduino/Teensy

--- config.h
+++ config.h
@@ -16,6 +16,11 @@
 
 #else
 
+#define DEC 10
+#define HEX 16
+#define OCT 8
+#define BIN 2
+
 #define millis __millis
 #define delay(milisec) __msleep(milisec)
 #define delayMicroseconds(usec) __usleep(usec)
